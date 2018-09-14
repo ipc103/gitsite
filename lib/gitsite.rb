1f0db2a4cbecf0fd214933ccbf0be64e03dc27fe
Ian Candy <iancandysemail@gmail.com>
2018-09-14 18:33:09 -0400

    create root directory

--- lib/gitsite.rb
+++ lib/gitsite.rb
@@ -1,5 +1,15 @@
-require "gitsite/version"
+require_relative "./gitsite/version"
+require 'fileutils'
 
 module Gitsite
-  # Your code goes here...
+  GH_PAGES_ROOT = 'doc'
+
+  def self.execute
+    puts "Finding or creating `doc` directory"
+    create_root_directory
+  end
+
+  def self.create_root_directory
+    FileUtils.mkdir_p GH_PAGES_ROOT
+  end
 end
--- spec/gitsite_spec.rb
+++ spec/gitsite_spec.rb
@@ -3,7 +3,22 @@ RSpec.describe Gitsite do
     expect(Gitsite::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.execute' do
+    let(:root_directory_name) { Gitsite::GH_PAGES_ROOT }
+
+    after :each do
+      FileUtils.rm_rf(root_directory_name)
+    end
+
+    it "outputs a message that it's created a docs directory" do
+      expect(STDOUT).to receive(:puts).with('Finding or creating `doc` directory')
+      Gitsite.execute
+    end
+
+    it "finds or creates a directory for GH Pages" do
+      FileUtils.mkdir_p root_directory_name
+      expect(FileUtils).to receive(:mkdir_p).with(root_directory_name)
+      Gitsite.execute
+    end
   end
 end
